3899205402c43ba3c3f3efe2649df3fff84b2335
alonzoalcantar <pinoa.03@outlook.com>
2022-11-27 22:20:56 -0700

    Poster Images display on Movie List Page

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -4,10 +4,18 @@ export default function MovieCard(movie) {
     return(
         <div>
             <Link to = {`/movies/${movie.title}`} >
-            <div class = 'item-card'>
+            <div 
+            class = 'item-card'
+            style={{
+                background: `url(${movie.posterPath})`,
+                backgroundRepeat:'no-repeat',
+                backgroundSize: 'contain'
+
+            }}
+            >
                 <div class = "movie-info">
                     <h1>{movie.title}</h1>
-                    <p><strong>{movie.releaseDate}</strong></p>
+                    <p>{movie.releaseDate}</p>
                 </div>
             </div>
 
--- src/MoviesListPage.jsx
+++ src/MoviesListPage.jsx
@@ -4,8 +4,13 @@ export default function MoviesListPage({movies}){
     return(
         <div>
             {movies.map((movie) => {
-                return <MovieCard title = {movie.title} movie = {movie}/>; 
-            })};
+                return <MovieCard 
+                title = {movie.title} 
+                movie = {movie} 
+                releaseDate = {movie.releaseDate}
+                posterPath = {movie.posterPath}
+                /> 
+            })}
         </div>
     )
 }
